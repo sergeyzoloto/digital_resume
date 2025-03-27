3e665d9cf6d6fd3193b607ec0dd3e38582d4891b
Sergey Zolotko <sergiozoloto@gmail.com>
2025-03-27 16:01:19 +0100

    add Card component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,9 +17,21 @@ function App() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          <p>count is {count}</p>
-        </Button>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+            <Button onClick={() => setCount((count) => count + 1)}>
+              <p>count is {count}</p>
+            </Button>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
