965caa96402e0b98345f3cba8f11ec338f81bcb4
Sergey Zolotko <sergiozoloto@gmail.com>
2025-04-02 18:23:17 +0200

    update Footer to adjust layout

--- react-app/src/components/footer.tsx
+++ react-app/src/components/footer.tsx
@@ -1,13 +1,13 @@
 export function Footer() {
   return (
-    <footer className="border-t bg-background">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+    <footer className="sticky bottom-0 z-50 w-full border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex flex-col items-center justify-around md:justify-between md:h-16 md:flex-row">
+        <div className="flex flex-col items-center gap-4 pt-2 md:flex-row md:gap-2 md:px-0 md:pt-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            © 2025 Sergey Zolotko. All rights reserved.
+            {new Date().getFullYear()}
           </p>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 pb-4 md:pb-0">
           <a
             href="#"
             target="_blank"
