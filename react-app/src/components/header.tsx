b93f411d323c7abf6e5a640c74ec5f1406927678
Sergey Zolotko <sergiozoloto@gmail.com>
2025-04-02 17:28:09 +0200

    adjust Header for full width layout

--- react-app/src/components/header.tsx
+++ react-app/src/components/header.tsx
@@ -13,7 +13,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="flex h-16 items-center justify-between w-full">
         <a href="/" className="font-bold text-xl px-4">
           Sergey Zolotko
         </a>
